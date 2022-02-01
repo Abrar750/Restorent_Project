acd5184f96532338eccb4207ee06d09dee099e9f
Abrar <siddiquiabrar620@gmail.com>
2022-02-01 10:09:56 -0800

    Restorent_Web

---
+++ restorent/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request,"index.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def service(request):
+    return render(request,"service.html")
+
+def menu(request):
+    return render(request,"menu.html")
+
+def team(request):
+    return render(request,"team.html")
+
+def testimonial(request):
+    return render(request,"testimonial.html")
+
+def booking(request):
+    return render(request,"booking.html")
+
+def contact(request):
+    return render(request,"contact.html")
